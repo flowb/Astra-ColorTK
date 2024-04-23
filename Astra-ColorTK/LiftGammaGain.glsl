8a1d6eabaad07f7b9e8fb0b7bf92151b6559498b
florian <flo@s2gfx.com>
2024-04-23 09:21:32 -0400

    fixed mtx ranges AGAIN :(

--- Astra-ColorTK/LiftGammaGain.glsl
+++ Astra-ColorTK/LiftGammaGain.glsl
@@ -1,7 +1,7 @@
 // title:   primary grading controls shader
 // author:  florian mosleh <flo@s2gfx.com>
-// version: 1.6
-// date:    20231105
+// version: 2.0
+// date:    20240423 (mtx range input fix)
 
 //@implements: sampler2D
 struct LiftGammaGain {
@@ -59,23 +59,23 @@ struct LiftGammaGain {
     //@ label: "offset B", editor: range, min: -10, max: 10, range_min: -10, range_max: 10, range_default: 0.0
     float offset_b;
 
-    //@ label: "RX1 0", editor: range, min: -1, max: 1, range_min: -1, range_max: 1, range_default: 0.4123908
+    //@ label: "RX1 0", editor: range, min: -2, max: 2, range_min: -2, range_max: 2, range_default: 0.4123908
     float RX1_0;
-    //@ label: "RX1 1", editor: range, min: -1, max: 1, range_min: -1, range_max: 1, range_default: 0.21263901
+    //@ label: "RX1 1", editor: range, min: -2, max: 2, range_min: -2, range_max: 2, range_default: 0.21263901
     float RX1_1;
-    //@ label: "RX1 2", editor: range, min: -1, max: 1, range_min: -1, range_max: 1, range_default: 0.01933082
+    //@ label: "RX1 2", editor: range, min: -2, max: 2, range_min: -2, range_max: 2, range_default: 0.01933082
     float RX1_2;
-    //@ label: "RX2 0", editor: range, min: -1, max: 1, range_min: -1, range_max: 1, range_default: 0.35758434
+    //@ label: "RX2 0", editor: range, min: -2, max: 2, range_min: -2, range_max: 2, range_default: 0.35758434
     float RX2_0;
-    //@ label: "RX2 1", editor: range, min: -1, max: 1, range_min: -1, range_max: 1, range_default: 0.71516868
+    //@ label: "RX2 1", editor: range, min: -2, max: 2, range_min: -2, range_max: 2, range_default: 0.71516868
     float RX2_1;
-    //@ label: "RX2 2", editor: range, min: -1, max: 1, range_min: -1, range_max: 1, range_default: 0.11919478
+    //@ label: "RX2 2", editor: range, min: -2, max: 2, range_min: -2, range_max: 2, range_default: 0.11919478
     float RX2_2;
-    //@ label: "RX3 0", editor: range, min: -1, max: 1, range_min: -1, range_max: 1, range_default: 0.18048079
+    //@ label: "RX3 0", editor: range, min: -2, max: 2, range_min: -2, range_max: 2, range_default: 0.18048079
     float RX3_0;
-    //@ label: "RX3 1", editor: range, min: -1, max: 1, range_min: -1, range_max: 1, range_default: 0.07219232
+    //@ label: "RX3 1", editor: range, min: -2, max: 2, range_min: -2, range_max: 2, range_default: 0.07219232
     float RX3_1;
-    //@ label: "RX3 2", editor: range, min: -1, max: 1, range_min: -1, range_max: 1, range_default: 0.95053215
+    //@ label: "RX3 2", editor: range, min: -2, max: 2, range_min: -2, range_max: 2, range_default: 0.95053215
     float RX3_2;
 
     //@ label: "WhiteK", editor: range, min: 1000, max: 8000, range_min: 1000, range_max: 8000, range_default: 6500
@@ -94,12 +94,12 @@ struct LiftGammaGain {
 
 };
 
-vec2 CCT_Kang2002(float temp) 
+vec2 CCT_Kang2002(float temp)
 {
 	float temp3 = pow(temp,3.0);
 	float temp2 = pow(temp,2.0);
 
-	float cie_x = 
+	float cie_x =
 		mix(
 			-0.2661239 * pow(10.0,9.0) / temp3
 			- 0.2343589 * pow(10.0,6.0) / temp2
@@ -115,7 +115,7 @@ vec2 CCT_Kang2002(float temp)
 	float x3 = pow(cie_x,3.0);
 	float x2 = pow(cie_x,2.0);
 
-	float cie_y = 
+	float cie_y =
 		mix(
 			-1.1063814 * x3 - 1.34811020 * x2 + 2.18555832 * cie_x - 0.20219683,
 			mix(
@@ -157,7 +157,7 @@ vec3 lMx(vec4 inColor, vec3 lumCOE, float lmix)
 vec4 texture(LiftGammaGain s, vec2 tex_coords) {
     vec4 o_color = texture(s.sampler, tex_coords);
     vec4 color = o_color;
-    
+
     vec4 lift = vec4(s.lift_r, s.lift_g, s.lift_b, s.lift_y);
     vec4 gamma = vec4(s.gamma_r, s.gamma_g, s.gamma_b, s.gamma_y);
     vec4 gain = vec4(s.gain_r, s.gain_g, s.gain_b, s.gain_y);
@@ -184,7 +184,7 @@ vec4 texture(LiftGammaGain s, vec2 tex_coords) {
 
     //saturation
 	outCol = mix(vec3(Y),outCol, s.Saturation);
-	
+
 	//contrast
 	outCol = mix(vec3(0.5),outCol, s.Contrast);
 
@@ -195,11 +195,11 @@ vec4 texture(LiftGammaGain s, vec2 tex_coords) {
 	outCol *= lMx( gain, yCOE, s.lmix );
 
 	// lift
-	outCol = outCol * ( 1.5 - 0.5 * lMx( lift, yCOE, s.lmix ) ) + 0.5 * lMx( lift, yCOE, s.lmix ) - 0.5;	
+	outCol = outCol * ( 1.5 - 0.5 * lMx( lift, yCOE, s.lmix ) ) + 0.5 * lMx( lift, yCOE, s.lmix ) - 0.5;
 
     //offset
     outCol += vec3( s.offset_r, s.offset_g, s.offset_b);
-	
+
     //prevent negative values
     outCol = max(vec3(0.0),outCol);
 
